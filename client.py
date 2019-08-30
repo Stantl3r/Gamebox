d99efebee69f95d5da628ec2518124ebc8a36d58
Stanley Tran <stanleyltran@gmail.com>
2019-08-30 15:08:58 -0700

    Cleaned up more

--- client.py
+++ client.py
@@ -2,22 +2,23 @@ import socket
 import cv2
 from video import stream
 
-stream_machine = socket.socket()
-host_name = socket.gethostbyname(socket.gethostname())
-port = 8082
-stream_machine.connect(('192.168.0.46', port))
-data = stream_machine.recv(4096)
-print("Message received: ", data.decode())
+if __name__ == "__main__":
+    stream_machine = socket.socket()
+    host_name = socket.gethostbyname(socket.gethostname())
+    port = 8082
+    stream_machine.connect(('192.168.0.46', port))
+    data = stream_machine.recv(4096)
+    print("Message received: ", data.decode())
 
-# Streaming video
-while True:
-    # Retrieves frame
-    frame = stream(stream_machine)
+    # Streaming video
+    while True:
+        # Retrieves frame
+        frame = stream(stream_machine)
 
-    # Displays frame
-    cv2.imshow("Video", frame)
-    if cv2.waitKey(1) == 27:
-        break
+        # Displays frame
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(1) == 27:
+            break
 
-cv2.destroyAllWindows()
-stream_machine.close()
+    cv2.destroyAllWindows()
+    stream_machine.close()
--- server.py
+++ server.py
@@ -1,32 +1,28 @@
 import socket
-import numpy as np
-import cv2
-from PIL import ImageGrab, Image
-import time
-import io
+from PIL import ImageGrab
 from video import convert
 
-port = 8082
-gaming_machine = socket.socket()
-host_name = socket.gethostbyname(socket.gethostname())
-gaming_machine.bind((host_name, port))
-gaming_machine.listen(1)
-print(host_name)
+if __name__ == "__main__":
+    port = 8082
+    gaming_machine = socket.socket()
+    host_name = socket.gethostbyname(socket.gethostname())
+    gaming_machine.bind((host_name, port))
+    gaming_machine.listen(1)
+    print(host_name)
 
-while True:
-    # Initializations
-    conn, addr = gaming_machine.accept()
-    conn.send("Testing connection . . .".encode())
-    image = ImageGrab.grab()
-
-    # Streaming video
     while True:
-        convert(image, gaming_machine, conn)
-
-        #Wait for receive message, then captures screen again
-        conn.recv(4096).decode()
+        # Initializations
+        conn, addr = gaming_machine.accept()
+        conn.send("Testing connection . . .".encode())
         image = ImageGrab.grab()
-        print("New Image")
 
+        # Streaming video
+        while True:
+            convert(image, gaming_machine, conn)
 
-    conn.close()
+            #Wait for receive message, then captures screen again
+            conn.recv(4096).decode()
+            image = ImageGrab.grab()
+            print("New Image")
+
+        conn.close()
