6e78cb9d71d359c79b81bdac9916d841b3db0af6
Stanley Tran <stanleyltran@gmail.com>
2019-08-30 14:41:48 -0700

    Testing on windows

--- client.py
+++ client.py
@@ -18,21 +18,32 @@ width, height = width.decode(), height.decode()
 print(width, height)
 
 # Streaming video
-global_size = int(stream_machine.recv(4096).decode())
+#global_size = int(stream_machine.recv(4096).decode())
+#global_size = int(stream_machine.recv(4096))
 while True:
-    data = b''
-    size = global_size
+    # data = b''
+    # size = global_size
+    # while size != 0:
+    #     read_size = stream_machine.recv(min(size, 40960000))
+    #     size -= len(read_size)
+    #     data += read_size
+    #     print(size)
+    size = int(stream_machine.recv(4096))
+    stream_machine.send("Got size".encode())
+    myfile = open("screen1", 'wb')
     while size != 0:
-        read_size = stream_machine.recv(min(size, 40960000))
-        size -= len(read_size)
-        data += read_size
         print(size)
+        data = stream_machine.recv(min(size, 40960000))
+        myfile.write(data)
+        size -= len(data)
+    myfile.close()
     stream_machine.send("Received".encode())
     print("Received")
-    image = Image.frombytes('RGBA', (int(width), int(height)), data)
-    image = np.array(image)
-    frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    cv2.imshow("Video", frame)
+    #image = Image.frombytes('RGBA', (int(width), int(height)), data)
+    img = Image.open("screen1")
+    image = np.array(img)
+    #frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cv2.imshow("Video", image)
     if cv2.waitKey(1) == 27:
         break
 
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from PIL import ImageGrab, Image
 import time
 import io
 
-port = 8080
+port = 8081
 gaming_machine = socket.socket()
 host_name = socket.gethostbyname(socket.gethostname())
 gaming_machine.bind((host_name, port))
@@ -26,21 +26,27 @@ while True:
     conn.send(str(height).encode())
 
     # Streaming video
-    conn.send(str(len(image.tobytes())).encode('utf-8').strip())
-    time.sleep(1)
-    print(str(len(image.tobytes())).encode())
+    # conn.send(str(len(image.tobytes())).encode('utf-8').strip())
+    # time.sleep(1)
+    # print(str(len(image.tobytes())).encode())
+    image.save("screen", format='PNG')
+    binary_img = open("screen", 'rb')
+    img_bytes = binary_img.read()
+    print(str(len(img_bytes)).encode())
+    conn.send(str(len(img_bytes)).encode())
+    conn.recv(4096).decode()
 
-    # img = Image.open(image, mode='r')
-
-    # imgByteArr = io.BytesIO()
-    # img.save(imgByteArr, format='PNG')
-    # imgByteArr = imgByteArr.getvalue()
-    # print(imgByteArr)
     while True:
         print("Sending")
-        conn.send(image.tobytes())
+        #conn.send(image.tobytes())
+        conn.send(img_bytes)
         if conn.recv(4096).decode() == "Received":
             image = ImageGrab.grab()
             print("New Image")
+            image.save("screen", format='PNG')
+            binary_img = open("screen", 'rb')
+            img_bytes = binary_img.read()
+            conn.send(str(len(img_bytes)).encode())
+            conn.recv(4096)
 
     conn.close()
